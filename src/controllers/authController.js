e063c4709fb060b3708c0a57fbe0ef6520c39474
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-07-05 20:53:42 -0300

    finalize auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,52 +1,13 @@
-import bcrypt from "bcrypt";
-import { createUser, searchUser } from "../repositories/userRepository.js";
+import * as service from "../services/authServices.js";
 
 export async function register(req, res) {
-  try {
     const { name, email, password } = req.body;
-
-    const existingUsers = await searchUser("email", email);
-
-    if (existingUsers.rowCount > 0) return res.sendStatus(409);
-  
-    const hashedPassword = bcrypt.hashSync(password, 12);
-
-    createUser(name, email, hashedPassword);
-
+    service.register(name, email, password);
     res.sendStatus(201);
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
 }
 
 export async function login(req, res) {
-  try {
     const { email, password } = req.body;
-
-    if (!email || !password) {
-      return res.sendStatus(422);
-    }
-
-    const { rows } = await searchUser("email", email);
-    const [user] = rows;
-
-    if (!user || !bcrypt.compareSync(password, user.password)) {
-      return res.sendStatus(401);
-    }
-
-    const token = jwt.sign(
-      {
-        id: user.id,
-      },
-      process.env.JWT_SECRET
-    );
-
-    res.send({
-      token,
-    });
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
+    const token = service.login(email, password);
+    res.send(token);
 }
--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,1 +1,28 @@
-export function validateRegister()
+import bcrypt from "bcrypt";
+import { createUser } from "../repositories/userRepository.js";
+
+export async function register(name, email, password){
+  const existingUsers = await searchUser("email", email);
+
+  if (existingUsers.rowCount > 0) 
+    throw new Error({type: "Conflict", message: "User already exists"});
+  
+  const hashedPassword=  bcrypt.hashSync(password, 12);
+  createUser(name, email, hashedPassword);
+}
+
+export async function login(email, password){
+  const existingUsers = await searchUser("email", email);
+
+  if (existingUsers.rowCount === 0) 
+    throw new Error({type: "Unauthorized", message: "User not found"});
+
+  const [user] = existingUsers.rows;
+  const { id, password: hashedPassword } = user;
+
+  if (!bcrypt.compareSync(password, hashedPassword)) 
+    throw new Error({type: "Unauthorized", message: "Invalid password"});
+
+  const token = jwt.sign({ id }, process.env.JWT_SECRET);
+  return { token };
+}
